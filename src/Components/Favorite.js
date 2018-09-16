77fccb8b8bbcf493ef7061d1922e9bf4e90652cb
dgiraldo313 <dgiraldo313@drew.edu>
2018-09-15 22:04:22 -0400

    #8 Added logic to handle favorite state for a beer that has been added to the favorite list in the past
    Using local storage to keep track of favored beers

--- src/Components/Favorite.js
+++ src/Components/Favorite.js
@@ -12,11 +12,19 @@ class Favorite extends Component {
     this.handleClick = this.handleClick.bind(this)
     this.addToFavorites = this.addToFavorites.bind(this)
     this.removeFromFavorites = this.removeFromFavorites.bind(this)
+    this.existInFavoriteList = this.existInFavoriteList.bind(this)
   }
 
   componentDidMount() {
     // Initiate FAVORITES in local storage, if it doesn't already result
     if ( typeof localStorage.FAVORITES === 'undefined' ) localStorage.setItem("FAVORITES", JSON.stringify([]))
+
+    // Check if current beer ID is in the favorite list and update state
+    if ( this.existInFavoriteList() ) {
+      this.setState({
+        isFavorite: true
+      })
+    }
   }
 
   handleClick() {
@@ -39,6 +47,7 @@ class Favorite extends Component {
   /*
   * Add favorite beer to local storage
   */
+
   addToFavorites() {
     const prevFavoriteList = JSON.parse( localStorage.getItem("FAVORITES") )
     //TODO expand items to save on local storage
@@ -55,6 +64,15 @@ class Favorite extends Component {
     // Remove id from list and return a clean list
     const newFavoriteList = prevFavoriteList.filter( favorite => favorite.beerID !== this.props.beerID )
     localStorage.setItem( "FAVORITES", JSON.stringify(newFavoriteList) )
+  }
+
+  /*
+  * Check if beer is in favorites list
+  */
+  existInFavoriteList() {
+    const favoriteList = JSON.parse( localStorage.getItem("FAVORITES") )
+    const matchedID = favoriteList.find( favorite => favorite.beerID === this.props.beerID )
+    return typeof matchedID !== 'undefined' ? true : false;
   }
 
   render() {
